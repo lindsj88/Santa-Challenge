4a320fb8b58f3c526a6b548fd15acb73a618bb93
Lindsey <Lindsey@Shadrack>
2015-11-11 18:29:34 -0700

    Merge origin/master
    Conflicts:
    	Santa-Challenge/src/santachallenge/SantaChallenge.java

--- Santa-Challenge/src/santachallenge/view/View.java
+++ Santa-Challenge/src/santachallenge/view/View.java
@@ -5,10 +5,51 @@
  */
 package santachallenge.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Lindsey
  */
 public abstract class View implements ViewInterface {
+    
+    private String promptMessage;
+    
+    @Override
+   public void display() {
+       String value = "";
+       boolean done = false;
+       
+       do {
+           System.out.println(this.promptMessage);
+           value = this.getInput();
+           done = this.doAction(value);
+           
+       } while (!done);
+       
+   }
+   
+   @Override
+   public String getInput() {
+        
+        boolean valid = false; //if menu selection has been retrieve
+        String value = null;
+        Scanner keyboard = new Scanner(System.in);
+        
+        while(!valid) { //while valid menu selection hasnt been retrieved
+            
+            //get menu selection from keyboard and trim off extra spaces
+            value = keyboard.nextLine();
+            value = value.trim();
+            
+            //if selection is invalid
+            if (value.length() < 1 ) {
+                System.out.println("You must enter a value");
+                continue;
+            }
+            break;
+          }
+        return value;   
+    }
     
 }
